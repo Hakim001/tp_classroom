c2df133b89ecb662d90b7322683b32ff72ab58ce
hakim <sobhak777@gmail.com>
2016-11-23 11:52:38 +0100

    ajout function générique

---
+++ ajaxP.js
@@ -0,0 +1,31 @@
+
+n ajaxPost(url, data, callback, isJson) {
+    
+    var req = new XMLHttpRequest();
+     
+    req.open("POST", url);
+    req.addEventListener("load", function (){
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            
+            callback(req.responseText);
+        }
+        else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    
+    });
+    
+    req.addEventListener("error", function (){
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    if(isJson) {
+        //Définit le contenu de la requête comme étant du JSON
+        req.setRequestHeader("Content-type", "application/json");
+        //Transforme la donnée du format Json vers le format texte avant
+        data = JSON.stringify(data);
+    }
+    
+    req.send(data);
+}
+
